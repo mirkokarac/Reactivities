0ac8c286feab84c715d0ef1812a0ce2f0933f65c
Mirko Karac <mirko@hotmail.com>
2023-07-20 11:02:48 +1000

    Refactor app with semantic elements
    - removed unused elements
    - removed console logs

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Button, Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -9,23 +10,20 @@ function App() {
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities')
       .then(response => {
-        console.log(response);
         setActivities(response.data);
     })
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          { activities.map((activity: any) => (
-            <li key={activity.id}>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+        { activities.map((activity: any) => (
+            <List.Item key={activity.id}>
               {activity.title}
-            </li>
-          ))}
-        </ul>
-      </header>
+            </List.Item>
+        ))}
+      </List>
     </div>
   );
 }
