4f8c950374fd2d101db666daa6d9e3bb9e2c9ce3
Mirko Karac <mirko@hotmail.com>
2023-07-20 11:32:23 +1000

    Refactor controller to use mediatR and new list activities query
    Individual activity put on hold atm. Will refactor later

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,27 +1,27 @@
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+using Application.Activities;
 
 namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    private readonly DataContext _context;
-    public ActivitiesController(DataContext context)
+    private readonly IMediator _mediator;
+    public ActivitiesController(IMediator mediator)
     {
-        _context = context;
+        _mediator = mediator;
     }
 
     [HttpGet] //api/activities
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await _context.Activities.ToListAsync();
+        return await _mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return await _context.Activities.FindAsync(id);
+        return Ok();
     }
 }
