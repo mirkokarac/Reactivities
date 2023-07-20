6c8619c04b22f657a4a4d1234febcb5c4da57993
Mirko Karac <mirko@hotmail.com>
2023-07-20 11:37:38 +1000

    Refactor activities controller to use mediatR from base controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,5 +1,4 @@
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Application.Activities;
 
@@ -7,16 +6,10 @@ namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    private readonly IMediator _mediator;
-    public ActivitiesController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [HttpGet] //api/activities
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await _mediator.Send(new List.Query());
+        return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
