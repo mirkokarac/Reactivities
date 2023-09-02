370897ea42ca737aa2e87f1f05f4e7892a051274
Mirko Karac <mirko@hotmail.com>
2023-09-02 17:15:40 +1000

    Refactor to handle deleting an activity

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -5,9 +5,11 @@ import { Button, Item, Label, Segment } from "semantic-ui-react";
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({activities, selectActivity}: Props) {
+export default function ActivityList({ activities, selectActivity,
+    deleteActivity }: Props) {
     return (
         <Segment>
             <Item.Group divided>
@@ -24,6 +26,10 @@ export default function ActivityList({activities, selectActivity}: Props) {
                                 <Button
                                     onClick={() => selectActivity(activity.id)}
                                     floated='right' content='View' color='blue'
+                                />
+                                <Button
+                                    onClick={() => deleteActivity(activity.id)}
+                                    floated='right' content='Delete' color='red'
                                 />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
