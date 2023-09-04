757ab68ba2dabfc37534a7d5ee05ba404e2c2d48
Mirko Karac <mirko@hotmail.com>
2023-09-04 12:30:06 +1000

    Refactor app to use agent to create/update an activity

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,11 +15,13 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({ activities, selectedActivity,
     selectActivity, cancelSelectActivity,
-    editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+    editMode, openForm, closeForm, createOrEdit, deleteActivity,
+    submitting }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -43,6 +45,7 @@ export default function ActivityDashboard({ activities, selectedActivity,
                         closeForm={closeForm}
                         activity={selectedActivity}
                         createOrEdit={createOrEdit}
+                        submitting={submitting}
                     />
                 }
             </Grid.Column>            
