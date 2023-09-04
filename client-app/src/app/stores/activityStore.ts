c890913de874dd5ca5cdbb146846e288a8b4157c
Mirko Karac <mirko@hotmail.com>
2023-09-04 16:04:08 +1000

    Refactor app to select activity using mobx
    - LoadingCmponent content made optional

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -4,7 +4,7 @@ import agent from "../api/agent";
 
 export default class ActivityStore {
     activities: Activity[] = [];
-    selectedActivity: Activity | null = null;
+    selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
     loadingInitial = false;
@@ -34,4 +34,21 @@ export default class ActivityStore {
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
     }
+
+    selectActivity = (id: string) => {
+        this.selectedActivity = this.activities.find(a => a.id === id);
+    }
+
+    cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
+    
+    openForm = (id?: string) => {
+        id ? this.selectActivity(id) : this.cancelSelectedActivity();
+        this.editMode = true;
+    }
+
+    closeForm = () => {
+        this.editMode = false;
+    }    
 }
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,16 +1,15 @@
 import React, { SyntheticEvent, useState } from "react";
 import { Activity } from "../../../app/models/activity";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
 
 interface Props {
     activities: Activity[];
-    selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
 }
 
-export default function ActivityList({ activities, selectActivity,
-    deleteActivity, submitting }: Props) {
+export default function ActivityList({ activities, deleteActivity, submitting }: Props) {
     const [target, setTarget] = useState('');
     
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id:string) {
@@ -18,6 +17,8 @@ export default function ActivityList({ activities, selectActivity,
         deleteActivity(id);
     }
 
+    const { activityStore } = useStore();
+    
     return (
         <Segment>
             <Item.Group divided>
@@ -32,7 +33,7 @@ export default function ActivityList({ activities, selectActivity,
                             </Item.Description>
                             <Item.Extra>
                                 <Button
-                                    onClick={() => selectActivity(activity.id)}
+                                    onClick={() => activityStore.selectActivity(activity.id)}
                                     floated='right' content='View' color='blue'
                                 />
                                 <Button
