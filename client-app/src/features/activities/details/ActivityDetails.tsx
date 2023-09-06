411cf3331abffb8c4a7b233ce39c4b878a36793e
Mirko Karac <mirko@hotmail.com>
2023-09-06 14:26:09 +1000

    Remove unused declerations

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,10 +1,9 @@
-import { Button, ButtonGroup, Card, Grid, Image } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { observer } from "mobx-react-lite";
 import { useParams } from "react-router";
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
 import ActivityDetailedHeader from "./ActivityDetailedHeader";
 import ActivityDetailsInfo from "./ActivityDetailsInfo";
 import ActivityDetailedChat from "./ActivityDetailedChat";
