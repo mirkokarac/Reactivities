50e1039b54c8512d2bd02f1b19f0cc9a2f1c94ce
Mirko Karac <mirko@hotmail.com>
2023-09-07 15:10:45 +1000

    Refactor agent 400 response to handle bad guid

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -14,10 +14,14 @@ axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 }, (error: AxiosError) => {
-    const { data, status } = error.response as AxiosResponse;
+    const { data, status, config } = error.response as AxiosResponse;
 
     switch (status) {
         case 400:
+            if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+                router.navigate('/not-found');
+            }
+
             if (data.errors) {
                 const modalStateErrors = [];
 
