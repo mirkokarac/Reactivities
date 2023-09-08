bf6485da97d5fb6816ba388e9501ef356cc56a85
Mirko Karac <mirko@hotmail.com>
2023-09-08 16:43:39 +1000

    Add date picker css
    file also got formatted

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,20 +1,21 @@
-import ReactDOM from 'react-dom/client';
-import 'semantic-ui-css/semantic.min.css';
-import 'react-calendar/dist/Calendar.css';
-import 'react-toastify/dist/ReactToastify.min.css';
-import './app/layout/styles.css';
-import reportWebVitals from './reportWebVitals';
-import { StoreContext, store } from './app/stores/store';
-import { RouterProvider } from 'react-router';
-import { router } from './app/router/Routes';
+import ReactDOM from "react-dom/client";
+import "semantic-ui-css/semantic.min.css";
+import "react-calendar/dist/Calendar.css";
+import "react-toastify/dist/ReactToastify.min.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "./app/layout/styles.css";
+import reportWebVitals from "./reportWebVitals";
+import { StoreContext, store } from "./app/stores/store";
+import { RouterProvider } from "react-router";
+import { router } from "./app/router/Routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <StoreContext.Provider value={store}>
     <RouterProvider router={router} />
-  </StoreContext.Provider>  
+  </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
