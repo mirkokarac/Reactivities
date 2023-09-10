d52fa4491a6234cad3bb4d419906081d12f77010
Mirko Karac <mirko@hotmail.com>
2023-09-10 13:41:23 +1000

    Refactor token service to use key from config

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -8,6 +8,12 @@ namespace API.Services;
 
 public class TokenService
 {
+    private readonly IConfiguration _config;
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+
     public string CreateToken(AppUser user)
     {
         var claims = new List<Claim>
@@ -17,7 +23,7 @@ public class TokenService
             new Claim(ClaimTypes.Email, user.Email),
         };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("KjJxzscwdsjT8v3Ep3m0a4xgDJMuVc8TxE6DmMGYXQTiNrXzaAPUGhmdu0ueww3PsP"));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
         var tokenDescriptor = new SecurityTokenDescriptor
