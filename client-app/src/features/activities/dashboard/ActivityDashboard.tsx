f1b5f29b1ba877745bc1e2bf9b81a06757b3deb5
Mirko Karac <mirko@hotmail.com>
2023-09-11 12:39:20 +1000

    Change loading component content and format the file

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,23 +7,24 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ActivityFilters from "./ActivityFilters";
 
 export default observer(function ActivityDashboard() {
-    const { activityStore } = useStore();
-    const { loadActivities, activityRegistry } = activityStore;
+  const { activityStore } = useStore();
+  const { loadActivities, activityRegistry } = activityStore;
 
-    useEffect(() => {
-        if (activityRegistry.size <= 1) loadActivities();
-    }, [loadActivities, activityRegistry.size]);
-  
-    if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />    
-    
-    return (
-        <Grid>
-            <Grid.Column width='10'>
-                <ActivityList />
-            </Grid.Column>
-            <Grid.Column width='6'>
-                <ActivityFilters />
-            </Grid.Column>            
-        </Grid>
-    )
-})
+  useEffect(() => {
+    if (activityRegistry.size <= 1) loadActivities();
+  }, [loadActivities, activityRegistry.size]);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading activities" />;
+
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList />
+      </Grid.Column>
+      <Grid.Column width="6">
+        <ActivityFilters />
+      </Grid.Column>
+    </Grid>
+  );
+});
