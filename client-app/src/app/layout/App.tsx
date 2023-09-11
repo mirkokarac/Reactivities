909af1c444110c8d117472143a8dc7573a1e9948
Mirko Karac <mirko@hotmail.com>
2023-09-11 12:27:42 +1000

    Refactor app to get user and show/hide loading component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,24 +1,41 @@
-import { Container } from 'semantic-ui-react';
-import NavBar from './NavBar';
-import { observer } from 'mobx-react-lite';
-import { Outlet, useLocation } from 'react-router';
-import HomePage from '../../features/home/HomePage';
-import { ToastContainer } from 'react-toastify';
+import { Container } from "semantic-ui-react";
+import NavBar from "./NavBar";
+import { observer } from "mobx-react-lite";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import { ToastContainer } from "react-toastify";
+import { useStore } from "../stores/store";
+import { useEffect } from "react";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
   const location = useLocation();
+  const { commonStore, userStore } = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore]);
+
+  if (!commonStore.appLoaded)
+    return <LoadingComponent content="Loading app..." />;
 
   return (
     <>
-      <ToastContainer position='bottom-right'  hideProgressBar theme='colored'/>
-      {location.pathname === '/' ? <HomePage /> : (
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
         <>
           <NavBar />
-          <Container style={{ marginTop: '7em' }}>
+          <Container style={{ marginTop: "7em" }}>
             <Outlet />
-          </Container>          
+          </Container>
         </>
-      )}      
+      )}
     </>
   );
 }
