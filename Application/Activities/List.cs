a84f6ad6c6d7be63cf86c1e8760db6c70de110fe
Mirko Karac <mirko@hotmail.com>
2023-09-12 14:46:33 +1000

    Refactor List to use ProjectTo
    remove unused code

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,5 +1,6 @@
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -24,13 +25,10 @@ public class List
         public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
         {
             var activities = await _context.Activities
-                .Include(a => a.Attendees)
-                .ThenInclude(u => u.AppUser)
+                .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
                 .ToListAsync();
 
-            var activitiesToREturn = _mapper.Map<List<ActivityDto>>(activities);
-
-            return Result<List<ActivityDto>>.Success(activitiesToREturn);
+            return Result<List<ActivityDto>>.Success(activities);
         }
     }
 }
