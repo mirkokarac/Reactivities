e5768d3b590594b09722a332a12a2f810019e625
Mirko Karac <mirko@hotmail.com>
2023-09-14 15:58:36 +1000

    Refactor profile model to contain photos
    - add Photo interface

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -5,6 +5,7 @@ export interface IProfile {
     displayName: string;
     image?: string;
     bio?: string;
+    photos?: Photo[];
 }
 
 export class Profile implements IProfile {
@@ -17,5 +18,12 @@ export class Profile implements IProfile {
     username: string;
     displayName: string;
     image?: string;
-    bio?: string;    
+    bio?: string;   
+    photos?: Photo[];
+}
+
+export interface Photo {
+    id: string;
+    url: string;
+    isMain: boolean;
 }
