2c2d6fd9a891409a310ec7abc8f23a91c9780929
Mirko Karac <mirko@hotmail.com>
2023-09-15 11:55:00 +1000

    Add profile photos component

---
+++ client-app/src/features/activities/profiles/ProfilePhotos.tsx
@@ -0,0 +1,21 @@
+import { Card, Header, Image, Tab } from "semantic-ui-react";
+import { Profile } from "../../../app/models/profile";
+
+interface Props {
+  profile: Profile;
+}
+
+export default function ProfilePhotos({ profile }: Props) {
+  return (
+    <Tab.Pane>
+      <Header icon="image" content="Photos" />
+      <Card.Group itemsPerRow={5}>
+        {profile.photos?.map((photo) => (
+          <Card key={photo.id}>
+            <Image src={photo.url} />
+          </Card>
+        ))}
+      </Card.Group>
+    </Tab.Pane>
+  );
+}
