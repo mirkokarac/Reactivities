775a29342489ecf0d84aa290a620ba74fd6c585e
Mirko Karac <mirko@hotmail.com>
2023-09-16 14:28:52 +1000

    Add set main photo function to profile store

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,5 +1,5 @@
 import { makeAutoObservable, runInAction } from "mobx";
-import { Profile } from "../models/profile";
+import { Photo, Profile } from "../models/profile";
 import agent from "../api/agent";
 import { store } from "./store";
 
@@ -7,6 +7,7 @@ export default class ProfileStore {
     profile: Profile | null = null;
     loadingProfile = false;
     uploading = false;
+    loading = false;
 
     constructor() {
         makeAutoObservable(this);
@@ -58,6 +59,31 @@ export default class ProfileStore {
             console.log(error);
 
             runInAction(() => this.uploading = false);
+        }
+    }
+
+    setMainPhoto =async (photo:Photo) => {
+        this.loading = true;
+
+        try {
+            await agent.Profiles.setMainPhoto(photo.id);
+            store.userStore.setImage(photo.url);
+
+            runInAction(() => {
+                if (this.profile && this.profile.photos) {
+                    this.profile.photos.find(p => p.isMain)!.isMain = false;
+                    this.profile.photos.find(p => p.id === photo.id)!.isMain = true;
+                    this.profile.image = photo.url;
+                    this.loading = false;
+
+                    // reload activities in order to update the image
+                    store.activityStore.loadActivities();
+                }
+            });
+        } catch (error) {
+            console.log(error);
+
+            runInAction(() => this.loading = false);
         }
     }
 }
