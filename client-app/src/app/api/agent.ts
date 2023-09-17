2a7c927c2f283c284719ae188668cd744a733b94
Mirko Karac <mirko@hotmail.com>
2023-09-17 12:16:38 +1000

    Add update profile endpoint to the agent

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -101,7 +101,8 @@ const Profiles = {
         });
     },
     setMainPhoto: (id: string) => requests.put(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`,)    
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`,),
+    updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile)
 }
 
 
