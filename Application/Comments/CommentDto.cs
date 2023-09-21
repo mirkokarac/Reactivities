e90d9ff14316d898bc98443a36cfbf07107ee0ac
Mirko Karac <mirko@hotmail.com>
2023-09-21 17:24:24 +1000

    Fix commentdto prop types

--- Application/Comments/CommentDto.cs
+++ Application/Comments/CommentDto.cs
@@ -4,9 +4,9 @@ public class CommentDto
 {
     public int Id { get; set; }
     public DateTime CreatedAt { get; set; }
-    public int Body { get; set; }
-    public int Username { get; set; }
-    public int DisplayName { get; set; }
-    public int Image { get; set; }
+    public string Body { get; set; }
+    public string Username { get; set; }
+    public string DisplayName { get; set; }
+    public string Image { get; set; }
 
 }
