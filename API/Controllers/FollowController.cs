ba4d8dd9271aaebd74a967c16e49b0ea19ed64dc
Mirko Karac <mirko@hotmail.com>
2023-09-22 12:52:17 +1000

    Add get followings controller

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -11,4 +11,11 @@ public class FollowController : BaseApiController
         return HandleResult(await Mediator.Send(
             new FollowToggle.Command { TargetUsername = username }));
     }
+
+    [HttpGet("{username}")]
+    public async Task<IActionResult> GetFollowings(string username, string predicate)
+    {
+        return HandleResult(await Mediator.Send(
+            new List.Query { Username = username, Predicate = predicate }));
+    }
 }
