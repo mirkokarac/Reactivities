2c99ff5dc83114cf69a08572ecbae015f5d7c577
Mirko Karac <mirko@hotmail.com>
2023-09-22 13:38:13 +1000

    Update other handlers with following property

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,7 +1,7 @@
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -16,16 +16,20 @@ public class List
     {
         private readonly DataContext _context;
         private readonly IMapper _mapper;
-        public Handler(DataContext context, IMapper mapper)
+        private readonly IUserAccessor _userAccessor;
+        public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
         {
+            _userAccessor = userAccessor;
             _mapper = mapper;
             _context = context;
         }
 
-        public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<Result<List<ActivityDto>>> Handle(Query request,
+            CancellationToken cancellationToken)
         {
             var activities = await _context.Activities
-                .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,
+                    new { currentUsername = _userAccessor.GetUserName() })
                 .ToListAsync();
 
             return Result<List<ActivityDto>>.Success(activities);
