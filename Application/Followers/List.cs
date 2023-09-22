ced31606bb0a28d5aa98503d44bc4697e26cec73
Mirko Karac <mirko@hotmail.com>
2023-09-22 13:23:22 +1000

    Update List to pass current user name as config to the mapping profile

--- Application/Followers/List.cs
+++ Application/Followers/List.cs
@@ -1,4 +1,5 @@
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
@@ -19,8 +20,10 @@ public class List
     {
         private readonly DataContext _context;
         private readonly IMapper _mapper;
-        public Handler(DataContext context, IMapper mapper)
+        private readonly IUserAccessor _userAccessor;
+        public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
         {
+            _userAccessor = userAccessor;
             _mapper = mapper;
             _context = context;
         }
@@ -37,14 +40,16 @@ public class List
                     profiles = await _context.UserFollowings
                         .Where(x => x.Target.UserName == request.Username)
                         .Select(u => u.Observer)
-                        .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider)
+                        .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider,
+                            new { currentUsername = _userAccessor.GetUserName() })
                         .ToListAsync();
                     break;
                 case "following":
                     profiles = await _context.UserFollowings
                         .Where(x => x.Observer.UserName == request.Username)
                         .Select(u => u.Target)
-                        .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider)
+                        .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider,
+                            new { currentUsername = _userAccessor.GetUserName() })
                         .ToListAsync();
                     break;
             }
