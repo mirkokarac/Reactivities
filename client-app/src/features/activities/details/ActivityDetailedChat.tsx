fa83431fe5bc4592ece220f2a40c0f8369aca906
Mirko Karac <mirko@hotmail.com>
2023-09-24 16:59:22 +1000

    Remove unused imports

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,12 +1,10 @@
 import { observer } from "mobx-react-lite";
-import { Segment, Header, Comment, Button, Loader } from "semantic-ui-react";
+import { Segment, Header, Comment, Loader } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Field, FieldProps, Form, Formik } from "formik";
-import MyTextIArea from "../../../app/common/form/MyTextArea";
 import * as Yup from "yup";
-import { posix } from "path";
 import { formatDistanceToNow } from "date-fns";
 
 interface Props {
